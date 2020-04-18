a844c789216e00c98ad6628f22478b2c69374163
Jocelynn Semer <jnsemer@gmail.com>
2020-04-18 15:31:33 -0400

    new build

---
+++ precache-manifest.eee1df3560de8933b9ee60636dcb44a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d96a9d5352e4bdfb2d5ccb1a06062452",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ba0296f069847dd9e863",
+    "url": "/static/css/main.0a2f2d3d.chunk.css"
+  },
+  {
+    "revision": "13fd2ab236fc52f01cf1",
+    "url": "/static/js/2.f53b094c.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.f53b094c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba0296f069847dd9e863",
+    "url": "/static/js/main.9fa34167.chunk.js"
+  },
+  {
+    "revision": "2bfb602158191270dfcd",
+    "url": "/static/js/runtime-main.ec4819b7.js"
+  }
+]);
